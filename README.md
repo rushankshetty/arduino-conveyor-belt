1762e187be2b3d7b375d6c3b6e6ab7319953007b
rushankshetty <shettyrushank@gmail.com>
2015-04-28 16:18:20 +0530

    Updated README and commented the code

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# automated-conveyor-belt
-Automated conveyor belt using arduino
+Automated Conveyor belt using Arduino UNO
+========
+
+
+* Sensors used:
+
+	- Obstacle sensor using a photodiode (for better accuracy I suggest using an ultrasonic obstacle sensor)
+
--- sketch_ConveyorBelt.ino
+++ sketch_ConveyorBelt.ino
@@ -1,10 +1,10 @@
 
-int LED = 13;
-int motor1Pin1 = 8;
-int motor2Pin2 = 9;
-int sensor = A0;
+int LED = 13;             // On-board led pin 13
+int motor1Pin1 = 8;       // Positive terminal of motor 1
+int motor2Pin2 = 9;       // Positive terminal of motor 2
+int sensor = A0;          // Obstacle sensor at A0 since it's analog
 int distance;
-int sensitivity = 700;
+int sensitivity = 700;    // Setting a threshold value
 
 void setup()
 {
@@ -19,16 +19,21 @@ void loop()
   distance = analogRead(sensor);
   if (distance < sensitivity)
   {
+    // If an object is not sensed, keep the conveyor belt in motion
     digitalWrite(LED, HIGH);
     digitalWrite(motor1Pin1, HIGH);
   }
   else
   {
+    // If an object is sensed, stop the conveyor belt for 5 seconds
+    // so that required action(s) can be performed
     digitalWrite(LED,LOW);
     digitalWrite(motor1Pin1, LOW);
     delay(5000);
   }
 
+  // Print the distance between the object and the sensor to the serial output
+  // for debugging purposes
   Serial.println(distance);
 
 }
